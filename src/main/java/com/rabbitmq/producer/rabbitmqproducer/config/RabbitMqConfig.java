9d9f88974ae5f27f9212330d13de4baba35aa83e
sohyun50 <sohyun50@aistudio.co.kr>
2022-09-27 09:56:54 +0900

    RabbiMQ - producer 예시

--- src/main/java/com/rabbitmq/producer/rabbitmqproducer/config/RabbitMqConfig.java
+++ src/main/java/com/rabbitmq/producer/rabbitmqproducer/config/RabbitMqConfig.java
@@ -9,8 +9,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class RabbitMqConfig {
     private static final String EXCHANGE_NAME = "test-exchange";
     private static final String QUEUE_NAME = "test-queue";
